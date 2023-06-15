6ec95a14df2d75ab6d43f90ca4d8c98ea315d346
Sergey Kivachuk <you@example.com>
2023-06-15 14:59:44 +0300

    знакомство с python

---
+++ main.py
@@ -0,0 +1,25 @@
+# print('Введите первое число: ')   # в столбик
+# a = input()
+
+# b = input('введите второе число: ')   #  в строчку 
+# print(a, '+',b,'=', a + b) 
+
+
+# c = 1 
+# print(c)
+# print(type(c))
+# c = bool(c)
+# print(c)
+# print(type(c))
+
+# print('Введите первое число: ')   # в столбик
+# a = int (input())
+
+# b = int  (input('введите второе число: '))   #  в строчку 
+# print(a, '+',b,'=', a + b) 
+  
+
+a = 'qwerty'
+
+for i in a:
+    print(i)
